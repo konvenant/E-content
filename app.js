534285f781c3834e72863ae3fa9d8c1a2f71ee6a
KONVENANT <sundaycovenant4@gmail.com>
2022-10-03 03:24:30 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const alert = require("alert");
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.set('view engine', 'ejs')
+
+
+app.get("/", function (req, res){
+  res.render("home");
+});
+app.get("/appendix", function (req, res){
+  res.render("appendix");
+});
+app.get("/appendf", function (req, res){
+  res.render("appendf");
+});
+app.get("/weekone", function (req, res){
+  res.render("weekone");
+});
+app.get("/weektwo", function (req, res){
+  res.render("weektwo");
+});
+app.get("/weekthree", function (req, res){
+  res.render("weekthree");
+});
+app.get("/weekfour", function (req, res){
+  res.render("weekfour");
+});
+app.get("/weekfive", function (req, res){
+  res.render("weekfive");
+});
+
+app.post("/", function(req,res){
+  var input = req.body.input;
+
+  if(input == "nelson" || input == "Nelson" || input == "NELSON"){
+    res.render("appendix")
+  } else{
+console.log("error");
+  }
+});
+
+
+app.listen(3000, function(){
+    console.log("Port started on Port 3000");
+})
---
+++ public/index.js
@@ -0,0 +1,7 @@
+
+var btn = document.getElementById("btn");
+var label =   document.getElementById("label");
+btn.addEventListener("click", function(){
+  label.style.display = "block";
+  label.style.color = "red";
+});
