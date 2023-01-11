736804d2d44fd287b0c72e303a5e09fcc81cb69a
KONVENANT <sundaycovenant4@gmail.com>
2023-01-11 18:38:08 +0100

    Initial commit

--- public/index.js
+++ public/index.js
@@ -3,3 +3,14 @@ document.getElementById("btn").addEventListener("click", myFunction );
 function myFunction() {
 setTimeout(() =>   document.getElementById("label").style.display = "block", 3000);
 }
+
+// var myVar;
+//
+// function myFunctions() {
+//   myVar = setTimeout(showPage, 3000);
+// }
+//
+// function showPage() {
+//   document.getElementById("loader").style.display = "none";
+//   document.getElementById("myDiv").style.display = "block";
+// }
